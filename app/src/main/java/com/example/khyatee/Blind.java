af0aa29b93269f7c49a057c05ecb971770953255
Khyatee Thakkar <khyatee.t@somaiya.edu>
2018-12-23 21:50:05 +0530

    Commit 1

---
+++ app/src/main/java/com/example/khyatee/Blind.java
@@ -0,0 +1,15 @@
+package com.example.khyatee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.khyatee.R;
+
+public class Blind extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blind);
+    }
+}
---
+++ app/src/main/java/com/example/khyatee/Guardian.java
@@ -0,0 +1,14 @@
+package com.example.khyatee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.khyatee.R;
+
+public class Guardian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guardian);
+    }
+}
